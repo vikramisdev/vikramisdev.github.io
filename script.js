be84a5899d7661881ee7dcbb346ab1edf4e81241
Vikram Vishwakarma <vs423502@gmail.com>
2023-02-25 14:54:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function animate_header() {
     
     let secondblock = document.getElementById("header-bottom");
     
-    firstblock.style.borderRadius = "0px 0px 400px 0px";
+    firstblock.style.borderRadius = "0px 0px 100% 0px";
     firstblock.style.transition = "1s border-radius";
     
-    secondblock.style.borderRadius = "400px 0px 0px 0px";
+    secondblock.style.borderRadius = "100% 0px 0px 0px";
     secondblock.style.transition = "1s border-radius";
 }
 
