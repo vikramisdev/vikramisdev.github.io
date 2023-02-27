f40c52b50950330e6fdf0cf89bfe2906644319fb
Vikram Vishwakarma <vs423502@gmail.com>
2023-02-27 16:28:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
-// animate header
+// navbar animation 
 
-function animate_header() {
-    let firstblock = document.getElementById("header-top");
+function toggle_navbar() {
+    let navbar = document.getElementById("navbar");
+    let all = document.getElementById("all");
+    let sidemenu = document.getElementById("sideMenu");
+    let sidemenusize;
     
-    let secondblock = document.getElementById("header-bottom");
+    nbca = navbar.style.transform;
+    let swidth = window.innerWidth;
+    let sheight = window.innerHeight;
     
-    firstblock.style.borderRadius = "0px 0px 100% 0px";
-    firstblock.style.transition = "1s border-radius";
-    
-    secondblock.style.borderRadius = "100% 0px 0px 0px";
-    secondblock.style.transition = "1s border-radius";
+    // resize side menu
+    if(swidth < sheight)
+        sidemenusize = "25%";
+    else if(swidth > sheight)
+        sidemenusize = "30%";
+    else
+        sidemenusize = "50%";
+
+
+    // toggle rotation of bar container
+    if(nbca == "" || nbca == "rotate(0deg)")
+    {
+        // rotate navbar from 0⁰ to 90⁰
+        navbar.style.transform = "rotate(90deg)";
+        // animate the rotation
+        navbar.style.transition = "1s transform";
+        
+        // show the side menu
+        sidemenu.style.paddingLeft = sidemenusize;
+        sidemenu.style.transition = "1s padding-left";
+        
+    }
+    else
+    {
+        // rotate navbar from 90⁰ to 0⁰
+        navbar.style.transform = "rotate(0deg)";
+        // animate the rotation
+        navbar.style.transition = "1s transform";
+        
+        // hide side menu
+        sidemenu.style.paddingLeft = "0px";
+        sidemenu.style.transition = "1s padding-left";
+    }
+        
 }
 
-// open url 
 
+// open urls
 function open_url(url) {
     window.open(url)
 }
