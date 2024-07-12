2a4c7a729b85a5a61e6e69e9c3026bca10706f41
vikramisdev <vs423502@gmail.com>
2024-07-12 18:13:26 +0530

    update

--- phone/script.js
+++ phone/script.js
@@ -11,3 +11,36 @@ function openUrl(url) {
 function openMail() {
   window.open("mailto:vs423502@gmail.com");
 }
+
+// jquery
+
+$(document).ready(function(){
+    // start animation when loaded
+    setTimeout(function(){
+          $(".profile").animate({
+            borderTopLeftRadius: "0px",
+            borderTopRightRadius: "0px",
+            borderBottomLeftRadius: "50px",
+            borderBottomRightRadius: "50px"
+        }, 500);
+    }, 800);
+    
+    $(".profile").on("touchstart", function(){
+        $(this).stop().animate({
+            borderTopLeftRadius: "0px",
+            borderTopRightRadius: "0px",
+            borderBottomLeftRadius: "0px",
+            borderBottomRightRadius: "0px"
+        }, 500);
+    });
+
+    $(".profile").on("touchend", function(){
+        $(this).stop().animate({
+            borderTopLeftRadius: "0px",
+            borderTopRightRadius: "0px",
+            borderBottomLeftRadius: "50px",
+            borderBottomRightRadius: "50px"
+        }, 500);
+    });
+});
+
