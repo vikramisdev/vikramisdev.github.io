842befdf5c2e5d30b2947a519eedf18128ae25b2
Vikram Vishwakarma <vs423502@gmail.com>
2024-08-26 12:46:46 +0530

    fix dark mode glitches

--- script.js
+++ script.js
@@ -1,105 +1,108 @@
+// Initialize AOS (Animate On Scroll)
 AOS.init({
   once: false,
   duration: 500,
   delay: 100,
 });
 
+// Open URL in a new tab
 function openUrl(url) {
-  window.open(url, (target = "_blank"));
+  window.open(url, "_blank");
 }
 
+// Open email client
 function openMail() {
   window.open("mailto:vs423502@gmail.com");
 }
 
-// init theme to light
-if (localStorage.getItem("theme") == null) {
+// Initialize theme to light if not set
+if (localStorage.getItem("theme") === null) {
   setTheme("light");
-
 }
 
+function setCookie(name, value, days=365) {
+  let expires = "";
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = `expires=${date.toUTCString()};`;
+  }
+  document.cookie = `${name}=${value};${expires}path=/`;
+}
+
+function deleteCookie(name) {
+  document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+}
+
+function getCookie(name) {
+  const nameEQ = `${name}=`;
+  const ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') c = c.substring(1);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+
+// Set theme in localStorage
 function setTheme(theme) {
-  localStorage.setItem("theme", theme);
+  setCookie("theme", theme);
+  applyTheme(theme);
 }
 
+// Get the current theme from localStorage
 function getTheme() {
-  return localStorage.getItem("theme");
+  return getCookie("theme");
 }
 
-// jquery
+// Apply theme styles based on the theme
+function applyTheme(theme) {
+  const isDark = theme === "dark";
+  $(".main-container").css("background", isDark ? "#232323" : "#ffffff");
+  $(".profile").css("background", isDark ? "#1E1E1E" : "#EBEBFA");
+  $(".bio").css("background", isDark ? "#323232" : "#F7F7FF");
+  $(".white-text").css("color", isDark ? "black" : "white");
+  $(".black-text").css("color", isDark ? "white" : "black");
+}
 
+// Toggle dark mode
+$(".dark-mode-container").click(function () {
+  var button = $(this).find("li");
+
+  // light theme
+  if(button.hasClass("bi-moon-fill")) {
+    button.removeClass("bi-moon-fill").addClass("bi-sun-fill");
+    $(this).css("justify-content", "flex-start");
+    setTheme("light");
+    applyTheme(getTheme());
+  } else if(button.hasClass("bi-sun-fill")) { // dark theme
+    button.removeClass("bi-sun-fill").addClass("bi-moon-fill");
+    $(this).css("justify-content", "flex-end");
+    setTheme("dark");
+    applyTheme(getTheme());
+  }
+});
+
+// Profile animation on load and touch events
 $(document).ready(function () {
-  // start animation when loaded
   setTimeout(function () {
-    $(".profile").animate(
-      {
-        borderTopLeftRadius: "0px",
-        borderTopRightRadius: "0px",
-        borderBottomLeftRadius: "50px",
-        borderBottomRightRadius: "50px",
-      },
-      500
-    );
+    $(".profile").animate({
+      borderTopLeftRadius: "0px",
+      borderTopRightRadius: "0px",
+      borderBottomLeftRadius: "50px",
+      borderBottomRightRadius: "50px",
+    }, 500);
   }, 800);
 
-  $(".profile").on("touchstart", function () {
-    $(this).stop().animate(
-      {
-        borderTopLeftRadius: "0px",
-        borderTopRightRadius: "0px",
-        borderBottomLeftRadius: "0px",
-        borderBottomRightRadius: "0px",
-      },
-      500
-    );
-  });
-
-  $(".profile").on("touchend", function () {
-    $(this).stop().animate(
-      {
-        borderTopLeftRadius: "0px",
-        borderTopRightRadius: "0px",
-        borderBottomLeftRadius: "50px",
-        borderBottomRightRadius: "50px",
-      },
-      500
-    );
+  $(".profile").on("touchstart touchend", function (event) {
+    const isTouchStart = event.type === "touchstart";
+    $(this).stop().animate({
+      borderTopLeftRadius: isTouchStart ? "0px" : "50px",
+      borderTopRightRadius: isTouchStart ? "0px" : "50px",
+      borderBottomLeftRadius: isTouchStart ? "0px" : "50px",
+      borderBottomRightRadius: isTouchStart ? "0px" : "50px",
+    }, 500);
   });
 });
-
-$(".dark-mode-container").click(function () {
-  const darkModeButton = $(this).find("li");
-
-  if (getTheme() === "dark") {
-    setTheme("light");
-    darkModeButton.toggleClass("bi-sun-fill bi-moon-fill");
-    $(this).css({
-      justifyContent: "flex-end",
-    });
-    applyDarkTheme(true);
-  } else {
-    setTheme("dark");
-    darkModeButton.toggleClass("bi-moon-fill bi-sun-fill");
-    $(this).css({
-      justifyContent: "flex-start",
-    });
-    applyDarkTheme(false);
-  }
-});
-
-function applyDarkTheme(status) {
-  if (status) {
-    $(".main-container").css("background", "#232323");
-    $(".profile").css("background", "#1E1E1E");
-    $(".bio").css("background", "#323232");
-    $(".white-text").css("color", "black");
-    $(".black-text").css("color", "white");
-  } else {
-    $(".main-container").css("background", "#ffffff");
-    $(".profile").css("background", "#EBEBFA");
-    $(".bio").css("background", "#F7F7FF");
-    $(".white-text").css("color", "white");
-    $(".black-text").css("color", "black");
-  }
-}
-
